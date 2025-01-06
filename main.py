ed284dfb4829b13cc21553236295700a65b0f047
Manas Khatore <94086185+Manas-Khatore@users.noreply.github.com>
2025-01-05 23:48:12 -0800

    testing post

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ app = FastAPI()
 # print("hello")
 # print(pk_types.type_categorize("Bulbasaur"))
 
-@app.get("/")
-async def root():
-    return {"message": pk_types.type_categorize("Bulbasaur")}
+@app.post("/")
+async def create_item(pokemon: str):
+    return pokemon
