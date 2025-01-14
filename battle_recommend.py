f55cd701c47c715ee856cb6a12a66bf11fda038b
Manas Khatore <94086185+Manas-Khatore@users.noreply.github.com>
2025-01-13 19:23:41 -0800

    testing strong_moves

--- battle_recommend.py
+++ battle_recommend.py
@@ -32,6 +32,7 @@ def strong_moves(pokemon_team, weak_pokemon, pok_type):
         print(pok)
         pok_moves_df = attacking_moves_df[attacking_moves_df["pokemon_name"] == pok]
         pok_strong_moves_df = pok_moves_df[pok_moves_df["type"].isin(pok_type_weaknesses)]
+        print(pok_strong_moves_df)
         strong_moves_df = pd.concat([strong_moves_df, pok_strong_moves_df], ignore_index=True)
     
     return strong_moves_df[["pokemon_name", "move_name", "type"]]
